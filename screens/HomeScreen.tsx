ffc651ba333bb7d62ee69a405b302ddfe6e90329
Alireza177 <alirezamahdavi015@gmail.com>
2024-09-30 13:31:16 +0200

    fix prettier

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,22 +2,25 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { RootStackParamlist } from '../navigators/RootStackNavigator';
 
-type Props = NativeStackScreenProps<RootStackParamlist, "Home">;
+type Props = NativeStackScreenProps<RootStackParamlist, 'Home'>;
 
 export default function HomeScreen({ navigation }: Props) {
-    return (
-        <View style={styles.container}>
-            <Text>HomeScreen</Text>
-            <Button title='Details' onPress={() => navigation.navigate('Details')}></Button>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <Button
+        title="Details"
+        onPress={() => navigation.navigate('Details')}
+      ></Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
